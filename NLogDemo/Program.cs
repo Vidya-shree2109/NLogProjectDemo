561c711c3da1c976189b54421fb15bc4024f398a
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-22 03:21:52 +0530

    [Vidya} Add : NLog Demo Project

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            AddNumbers number = new AddNumbers();
+            number.Sum(1, 2);
+            Console.WriteLine("The sum of two numbers is " + number.Sum(1, 2));
+        }
+    }
+}
